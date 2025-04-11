563a1075fbd0befe2e33cc3748c02309f7ba6ca3
kartic71197 <karticmalhotra3289@gmail.com>
2025-04-11 14:51:17 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [countries, setCountries] = useState([]);
+  const [states, setStates] = useState([]);
+  const [cities, setCities] = useState([]);
+
+  const [selectedCountry, setSelectedCountry] = useState("");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [showDiv, setShowDiv] = useState(false);
+
+
+  useEffect(() => {
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    const response = await fetch("https://crio-location-selector.onrender.com/countries");
+    const data = await response.json();
+    setCountries(data);
+  };
+
+  const fetchStates = async (e) => {
+    const country = e.target.value;
+    setSelectedCountry(country);
+    setCities([]);
+    const response = await fetch(`https://crio-location-selector.onrender.com/country=${country}/states`);
+    const data = await response.json();
+    setStates(data);
+  };
+
+  const fetchCities = async (e) => {
+    const state = e.target.value;
+    setSelectedState(state);
+    const response = await fetch(
+      `https://crio-location-selector.onrender.com/country=${selectedCountry}/state=${state}/cities`
+    );
+    const data = await response.json();
+    setCities(data);
+  };
+
+  function showData(e){
+    const city = e.target.value;
+    setSelectedCity(city);
+    if (city) {
+      setShowDiv(true);
+    }
+  }
+
+  return (
+    <>
+      <select onChange={fetchStates}>
+        <option value="">Select Country</option>
+        {countries.map((country) => (
+          <option key={country} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+
+      <select onChange={fetchCities}>
+        <option value="">Select State</option>
+        {states.map((state) => (
+          <option key={state} value={state}>
+            {state}
+          </option>
+        ))}
+      </select>
+
+      <select onChange={showData}>
+        <option value="">Select City</option>
+        {cities.map((city) => (
+          <option key={city} value={city}>
+            {city}
+          </option>
+        ))}
+      </select>
+
+      {
+        showDiv && <h2>Your selected {selectedCity},{selectedState},{selectedCountry}</h2>
+
+        
+      }
+    </>
+  );
+}
+
+export default App;
